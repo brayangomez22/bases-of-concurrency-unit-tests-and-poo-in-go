48c850ca7a92a09fe35c75722980798a3ba18fce
Brayan Gómez <brayan.gomez@sofka.com.co>
2023-05-17 13:55:12 -0500

    first commit

---
+++ poo/inheritance/class.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+type Employee struct {
+	id int
+}
+
+type FullTimeEmployee struct {
+	Person
+	Employee
+}
+
+func GetMessage(p Person) {
+	fmt.Printf("%s with age %d\n", p.name, p.age)
+}
+
+func main() {
+	ftEmployee := FullTimeEmployee{}
+	ftEmployee.name = "Name"
+	ftEmployee.age = 2
+	ftEmployee.id = 5
+	fmt.Printf("%v\n", ftEmployee)
+	//GetMessage(ftEmployee)
+}
